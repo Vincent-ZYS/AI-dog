02b4e53b00926acf2ad2234506c0aaf0976cec37
Frank-Zou <zyswwang@qq.com>
2018-04-06 19:58:47 +0800

    1

---
+++ Assets/Scripts/Passerby/PasserbyMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasserbyMove : MonoBehaviour {
+
+    private GameObject[] allPositions;
+    private float[] allDistances;
+    private int currentPosition = 0;
+    private float minDistance;
+
+    public float walkSpeed;
+
+
+	void Start()
+	{
+        allPositions = GameObject.FindGameObjectsWithTag("position");
+        allDistances = new float[allPositions.Length];
+	}
+
+    void Update()
+    {
+        Invoke("searchDistance",minDistance/(Time.deltaTime * walkSpeed));
+        
+        PMoveBehaviour(allPositions[currentPosition].transform.position);
+
+        if (Vector3.Distance(transform.position, allPositions[currentPosition].transform.position) <= 2)
+        {
+            currentPosition++;
+        }
+        if (currentPosition == allPositions.Length)
+        {
+            currentPosition = 0;
+        } 
+	}
+
+    public void PMoveBehaviour(Vector3 nextAim)
+    {
+        transform.LookAt(nextAim);
+        transform.position = Vector3.Lerp(transform.position, nextAim, Time.deltaTime * walkSpeed);
+    }
+
+    public void searchDistance()
+    {
+        minDistance = allDistances[0];
+        for (int i = 0; i < allPositions.Length; i++)
+        {
+            allDistances[i] = Vector3.Distance(transform.position, allPositions[i].transform.position);
+            if (minDistance > allDistances[i])
+            {
+                minDistance = allDistances[i];
+                currentPosition = i;
+            }
+        }
+    }
+}
